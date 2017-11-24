1bc31ca76483725d07d8393553ffba2c02573172
gabrieleminniti <gabriele.minniti2@gamil.com>
2017-11-24 23:12:05 +0100

    update

--- app.py
+++ app.py
@@ -15,28 +15,29 @@ def index():
         immagine = ImageGrab.grab()
         half_the_width = immagine.size[0] / 2
         half_the_height = immagine.size[1] / 2
-        img4 = immagine.crop(
-                (half_the_width - 790,
-                half_the_height - 450,
-                half_the_width + 790,
-                half_the_height + 450)
-                )
-        img_loaded = np.array(img4)
-        resized = cv2.resize(img_loaded, (256, 256))
-        blue = resized[:,:,0]
-        green = resized[:, :, 1]
-        red = resized[:, : , 2]
-        immagine_2 = cv2.merge((blue, green, red))
-        x = immagine_2.reshape((1, ) + immagine_2.shape)
-        softmax = load_model('ULTIMO_MODELLO_V3.h5')
-        softmax.load_weights('ULTIMO_MODELLO_V3_pesi.h5')
-        previsione = softmax.predict(x)
-        if previsione[0,0] > .3:
-            risultato =  'si tratta di: Borealis '
-        elif previsione[0,1] >.3:
-            ritultato =  'si tratta di Duffel '
-        elif previsione[0,2] > .3:
-            risultato ='si tratta di: Thermoball '
+            #img4 = immagine.crop(
+            #   (half_the_width - 790,
+            #    half_the_height - 450,
+            #    half_the_width + 790,
+            #    half_the_height + 450)
+            #    )
+            #img_loaded = np.array(img4)
+            #resized = cv2.resize(img_loaded, (256, 256))
+            #blue = resized[:,:,0]
+            #green = resized[:, :, 1]
+            # red = resized[:, : , 2]
+            #immagine_2 = cv2.merge((blue, green, red))
+            #x = immagine_2.reshape((1, ) + immagine_2.shape)
+            #softmax = load_model('ULTIMO_MODELLO_V3.h5')
+            #softmax.load_weights('ULTIMO_MODELLO_V3_pesi.h5')
+        #previsione = softmax.predict(x)
+        #if previsione[0,0] > .3:
+        #    risultato =  'si tratta di: Borealis '
+        #elif previsione[0,1] >.3:
+        #    ritultato =  'si tratta di Duffel '
+        #elif previsione[0,2] > .3:
+        #   risultato ='si tratta di: Thermoball '
+        risultato = immagine.size
         return render_template('index.html', val = risultato)
     else:
         #request.method == 'GET':

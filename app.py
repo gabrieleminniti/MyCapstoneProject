67a5df34f1f41814ec4bc5dd331b612efc1a0561
gabrieleminniti <32270693+gabrieleminniti@users.noreply.github.com>
2017-11-26 15:12:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,46 +3,40 @@ import keras
 from keras.models import load_model
 import numpy as np
 import cv2
-import os
-import pyscreenshot as ImgaGrab
+
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 
+
 def index():
     if request.method == 'POST':
-        screen =ImageGrab.grab()
-        half_the_width = screen.size[0] / 2
-        half_the_height = screen.size[1] / 2
-        img4 = screen.crop(
-              (half_the_width - 790,
-                half_the_height - 450,
-                half_the_width + 790,
-                half_the_height + 450)
-                )
-        img_loaded = np.array(img4)
-        resized = cv2.resize(img_loaded, (256, 256))
-        blue = resized[:,:,0]
-        green = resized[:, :, 1]
-        red = resized[:, : , 2]
-        immagine_2 = cv2.merge((blue, green, red))
-        x = immagine_2.reshape((1, ) + immagine_2.shape)
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
-        softmax.load_weights('ULTIMO_MODELLO_V3_pesi.h5')
-        previsione = softmax.predict(x)
-        if previsione[0,0] > .3:
-            risultato =  'si tratta di: Borealis '
-        elif previsione[0,1] >.3:
-            ritultato =  'si tratta di Duffel '
-        elif previsione[0,2] > .3:
-           risultato ='si tratta di: Thermoball '
-        return render_template('index.html', val = risultato)
+        cap = cv2.VideoCapture('Base_camp.mp4')
+        count = 0
+        my_list = []
+        while count <10:
+            ret, frame = cap.read()
+            my_list.append( frame)
+            count += 1
+        my_try = []
+        for el in my_list:
+            uno =  cv2.resize(el, (256, 256))
+            due = uno.reshape((1,) + uno.shape)
+            prev = softmax.predict(due)
+            if prev[0,0] > .3:
+                my_try.append('borealis')
+            elif prev[0,1] >.3:
+                my_try.append('duffel')
+            elif prev[0,2] > .3:
+                    my_try.append('thermoball')
+        return render_template('index.html', val = my_try)
     else:
         #request.method == 'GET':
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,  host='0.0.0.0', port=port) #,
+    app.run(debug=True) #,port = int(os.environ.get("PORT", 5000)) ,  host='0.0.0.0', port=port
+

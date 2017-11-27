f2da332309d72106deb94b65f3c0bbab09a5e3f3
gabrieleminniti <gabriele.minniti2@gamil.com>
2017-11-27 12:49:07 +0100

    update

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def index():
         im.save('screenshot.png')
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         #img = ImageGrab.grab()
-        img = cv2.imread('screenshot.png',3)
-        resized = cv2.resize(img, (256, 256))
-        resized_2 = resized.reshape((1,) + resized.shape)
-        ris = softmax.predict(resized_2)
+        #img = cv2.imread('screenshot.png',3)
+        #resized = cv2.resize(img, (256, 256))
+        #resized_2 = resized.reshape((1,) + resized.shape)
+        #ris = softmax.predict(resized_2)
         ritorno = im.size
         return render_template('index.html', val = ritorno)
     else:

4bc59dc23447b317bdba551a900704354d4eda45
gabrieleminniti <gabriele.minniti2@gamil.com>
2017-11-28 15:11:13 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template,request
 import keras
 from keras.models import load_model
-import pyscreenshot as ImageGrab
 import numpy as np
 import cv2
 from PIL import Image
@@ -14,15 +13,20 @@ app = Flask(__name__)
 
 def index():
     if request.method == 'POST':
-        im=ImageGrab.grab()
-        im.save('data/screenshot.png')
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
-        img = ImageGrab.grab()
-        img = cv2.imread('screenshot.png',3)
+        softmax.load_weights('ULTIMO_MODELLO_V3_pesi.h5')
+        img = cv2.imread('data/test.png',3)
         resized = cv2.resize(img, (256, 256))
         resized_2 = resized.reshape((1,) + resized.shape)
         ris = softmax.predict(resized_2)
-        ritorno = im.size
+        if ris[0,0]>.3:
+            ritorno = 'borealis'
+        elif ris[0,1] > .3:
+            ritorno = 'duffel'
+        elif ris[0,2] >.3:
+            ritorno = 'duffel'
+        else:
+            ritorno = 'no idea'
         return render_template('index.html', val = ritorno)
     else:
         #request.method == 'GET':

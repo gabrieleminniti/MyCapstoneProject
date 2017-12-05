c9b05e66d72ca8de0d622ddf0fadbbb19a17e5d6
gabrieleminniti <gabriele.minniti2@gamil.com>
2017-12-05 21:55:08 +0100

    update

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def index():
         return render_template('index.html', val = ritorno)
 
 
-# video duffel_1
+# video duffel_1 --> durata 4.05
 @app.route('/primo_video',methods=['GET','POST'])
 def next_gabri_1():#can't have two functions with the same name
     if request.method == 'GET':
@@ -71,21 +71,27 @@ def next_gabri_1():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        match = re.findall(r'[0-9]', a) #sistemare  qui
-        uno = match[1]
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_duf/duf_2.pkd' ,'r'))
-        #ritorno = nuovo_diz[t]
-            ritorno = uno
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 5:
+                ritorno = 'the video is about four minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 246:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_duf/duf_2.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('primo_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('primo_video.html', num = ritorno)
 
-#video thermoball_1
+#video thermoball_1 --> 2.33
 @app.route('/secondo_video',methods=['GET','POST'])
 def next_gabri_2():#can't have two functions with the same name
     if request.method == 'GET':
@@ -95,16 +101,26 @@ def next_gabri_2():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_ther/ther_4.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 3:
+                ritorno = 'the video is about two minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 153:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_ther/ther_4.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('secondo_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('secondo_video.html', num = ritorno)
+
 
 #video borealis_1
 @app.route('/terzo_video',methods=['GET','POST'])
@@ -116,18 +132,30 @@ def next_gabri_3():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_bor/bor_2.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 5:
+                ritorno = 'the video is about four minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 274:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_bor/bor_2.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('terzo_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('terzo_video.html', num = ritorno)
 
-#video borealis_2
+
+
+
+#video borealis_2 --> 1.02
 @app.route('/quarto_video',methods=['GET','POST'])
 def next_gabri_4():#can't have two functions with the same name
     if request.method == 'GET':
@@ -137,18 +165,28 @@ def next_gabri_4():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_bor/bor_13.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 2:
+                ritorno = 'the video is about one'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 63:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_bor/bor_13.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('quarto_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('quarto_video.html', num = ritorno)
 
-#video duffel_2
+
+#video duffel_2 --> 1.17
 @app.route('/quinto_video',methods=['GET','POST'])
 def next_gabri_5():#can't have two functions with the same name
     if request.method == 'GET':
@@ -158,16 +196,26 @@ def next_gabri_5():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_duf/duf_3.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 2:
+                ritorno = 'the video is about one minute'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 77:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_duf/duf_3.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('quinto_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('quinto_video.html', num = ritorno)
+
 
 #video thermoball_2
 @app.route('/sesto_video',methods=['GET','POST'])
@@ -176,22 +224,32 @@ def next_gabri_6():#can't have two functions with the same name
         return render_template('sesto_video.html', num = '')
     else:
         # method = POST
-        app.a = request.form['sec']
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
+        app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_ther/ther_5.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 3:
+                ritorno = 'the video is about two minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 121:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_ther/ther_5.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('sesto_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('sesto_video.html', num = ritorno)
 
 
-#video thermoball_3
+
+#video thermoball_3  --> 2.36
 @app.route('/settimo_video',methods=['GET','POST'])
 def next_gabri_7():#can't have two functions with the same name
     if request.method == 'GET':
@@ -201,18 +259,28 @@ def next_gabri_7():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_ther/daai.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 3:
+                ritorno = 'the video is about two minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 156:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_ther/daai.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('settimo_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('settimo_video.html', num = ritorno)
 
-#video borealis_3
+
+#video borealis_3 --> 3.41
 @app.route('/ottavo_video',methods=['GET','POST'])
 def next_gabri_8():#can't have two functions with the same name
     if request.method == 'GET':
@@ -222,16 +290,25 @@ def next_gabri_8():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_bor/bor_3.pkd' ,'r')) #salvarlo così
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 4:
+                ritorno = 'the video is about three minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 222:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_bor/bor_3.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('ottavo_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('ottavo_video.html', num = ritorno)
 
 
 
@@ -245,16 +322,25 @@ def next_gabri_9():#can't have two functions with the same name
         softmax = load_model('ULTIMO_MODELLO_V3.h5')
         app.a = request.form['sec']
         a = app.a.encode('ascii','ignore').lower()
-        if a != '':
-            temp = float(a)
-            minuti = int(temp)
-            secondi = (temp-minuti) * 100
-            t = int((minuti * 60) + secondi)
-            nuovo_diz = dill.load(open('data/data_duf/duf_4.pkd' ,'r'))
-            ritorno = nuovo_diz[t]
+        match = re.findall(r'\d+', a)
+        if match != []:
+            minuti = int(match[0]) #minuti
+            if minuti >= 9:
+                ritorno = 'the video is about eight minutes'
+            else:
+                secondi = int(match[1]) #secondi
+                if secondi >59:
+                    ritorno = 'a minute has maximum 59 seconds'
+                else:
+                    idx = (minuti * 60) + secondi
+                    if idx >= 522:
+                        ritorno = 'insert a valid moment'
+                    else:
+                        nuovo_diz = dill.load(open('data/data_bor/duf_4.pkd' ,'r'))
+                        ritorno = nuovo_diz[idx]
         else:
-            ritorno =  'insert something'
-        return render_template('nono_video.html', num = ritorno)
+            ritorno = 'you fail to insert the moment that you wanna recognize'
+    return render_template('nono_video.html', num = ritorno)
 
 
 @app.route('/info', methods = ['GET', 'POST'])
